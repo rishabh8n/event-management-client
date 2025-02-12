f901e42ebc31c8a2a3d9d19d3d15c8d3411392eb
Rishabh <rishabh8n@gmail.com>
2025-02-12 21:03:50 +0000

    fix api url issue

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000" + "/api",
+  baseURL:
+    import.meta.env.VITE_API_URL + "/api" || "http://localhost:8000" + "/api",
   headers: {
     "Content-Type": "application/json",
   },
